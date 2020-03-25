c08c8763c3f39d58fcf775e433229fd78a1a7d91
Emily Xie <emilyxxie@gmail.com>
2020-03-25 17:01:35 -0400

    Start writing testing infra

---
+++ model_testing.py
@@ -0,0 +1,5 @@
+"""
+This script generates and populates a CSV file containing the outputs
+of our baseline model and our developed model, which will be displayed in the CLI
+so that we can compare the results.
+"""
