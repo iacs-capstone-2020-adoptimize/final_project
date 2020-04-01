8bd6924b3111f692f8e0f824f3b4a1f389855a02
Andrew Fu <andrewfu77@gmail.com>
2020-04-01 01:06:34 -0400

    Retrained yolov3 with additional head feature

--- yolo_training/Data/Source_Images/extract_feature_box.py
+++ yolo_training/Data/Source_Images/extract_feature_box.py
@@ -10,6 +10,13 @@ our YOLO neural net.
 """
 
 import math
+# These classes should match data_classes.txt
+CLASS_EYE = 0
+CLASS_NOSE = 1
+CLASS_EAR = 2
+CLASS_HEAD = 3
+
+
 def get_points(path):
     """
     Gets feature points based on annontation file, returning a tuple for each feature.
@@ -20,7 +27,8 @@ def get_points(path):
             raise ValueError
         else:
             split_str = string.split()[1:]
-            results =  tuple((int(loc_x), int(loc_y)) for loc_x, loc_y in zip(split_str[::2], split_str[1::2]))
+            results = tuple((int(loc_x), int(loc_y))
+                            for loc_x, loc_y in zip(split_str[::2], split_str[1::2]))
             for result in results:
                 if result[0] < 0 or result[1] < 0:
                     raise ValueError
@@ -40,16 +48,25 @@ def get_points(path):
 
 def get_ears(feature_points):
     # Left ear
-    x_min_left = min(feature_points[3][0], feature_points[4][0], feature_points[5][0])
-    x_max_left = max(feature_points[3][0], feature_points[4][0], feature_points[5][0])
-    y_min_left = min(feature_points[3][1], feature_points[4][1], feature_points[5][1])
-    y_max_left = max(feature_points[3][1], feature_points[4][1], feature_points[5][1])
+    x_min_left = min(feature_points[3][0],
+                     feature_points[4][0], feature_points[5][0])
+    x_max_left = max(feature_points[3][0],
+                     feature_points[4][0], feature_points[5][0])
+    y_min_left = min(feature_points[3][1],
+                     feature_points[4][1], feature_points[5][1])
+    y_max_left = max(feature_points[3][1],
+                     feature_points[4][1], feature_points[5][1])
     # Right ear
-    x_min_right = min(feature_points[6][0], feature_points[7][0], feature_points[8][0])
-    x_max_right = max(feature_points[6][0], feature_points[7][0], feature_points[8][0])
-    y_min_right = min(feature_points[6][1], feature_points[7][1], feature_points[8][1])
-    y_max_right = max(feature_points[6][1], feature_points[7][1], feature_points[8][1])
-    return (x_min_left, y_min_left, x_max_left, y_max_left, 2), (x_min_right, y_min_right, x_max_right, y_max_right, 2)
+    x_min_right = min(feature_points[6][0],
+                      feature_points[7][0], feature_points[8][0])
+    x_max_right = max(feature_points[6][0],
+                      feature_points[7][0], feature_points[8][0])
+    y_min_right = min(feature_points[6][1],
+                      feature_points[7][1], feature_points[8][1])
+    y_max_right = max(feature_points[6][1],
+                      feature_points[7][1], feature_points[8][1])
+    return ((x_min_left, y_min_left, x_max_left, y_max_left, CLASS_EAR),
+            (x_min_right, y_min_right, x_max_right, y_max_right, CLASS_EAR))
 
 
 def get_eyes_and_nose(feature_points):
@@ -70,7 +87,17 @@ def get_eyes_and_nose(feature_points):
     # Now, let's get mouths
     mouth = feature_points[2]
 
-    return ((left_eye[0] - radius, left_eye[1] - radius, left_eye[0] + radius, left_eye[1] + radius, 0), (right_eye[0] - radius, right_eye[1] - radius, right_eye[0] + radius, right_eye[1] + radius, 0), (mouth[0] - (2 * radius), mouth[1] - radius, mouth[0] + (2 * radius), mouth[1] + radius, 1))
+    return ((left_eye[0] - radius, left_eye[1] - radius, left_eye[0] + radius, left_eye[1] + radius, CLASS_EYE),
+            (right_eye[0] - radius, right_eye[1] - radius, right_eye[0] + radius, right_eye[1] + radius, CLASS_EYE),
+            (mouth[0] - (2 * radius), mouth[1] - radius, mouth[0] + (2 * radius), mouth[1] + radius, CLASS_NOSE))
+
+
+def get_head(feature_points):
+    x_min = min(feature[0] for feature in feature_points)
+    x_max = max(feature[0] for feature in feature_points)
+    y_min = min(feature[1] for feature in feature_points)
+    y_max = max(feature[1] for feature in feature_points)
+    return (x_min, y_min, x_max, y_max, CLASS_HEAD)
 
 
 if __name__ == "__main__":
@@ -87,16 +114,16 @@ if __name__ == "__main__":
                     print("Found negative value; skipping image")
                     continue
 
-                # For now, we're going to try to detect the various features of a cat instead of the whole
-                # head, so we will comment out the line of code grabbing the entire head.
-                # head_str = ",".join(str(point) for point in get_head(points)) + ",0
-
                 # These are the points for the bounding boxes
-                left_eye, right_eye, nose = [",".join(str(point) for point in feature) for feature in get_eyes_and_nose(points)]
-                left_ear, right_ear = [",".join(str(point) for point in ear) for ear in get_ears(points)]
+                left_eye, right_eye, nose = [",".join(
+                    str(point) for point in feature) for feature in get_eyes_and_nose(points)]
+                left_ear, right_ear = [
+                    ",".join(str(point) for point in ear) for ear in get_ears(points)]
+                head = ",".join(str(point) for point in get_head(points))
 
                 output_list.append(" ".join(
-                    ("/".join((cwd, dir_name, file_name[:-4])), left_ear, right_ear, left_eye, right_eye, nose),
+                    ("/".join((cwd, dir_name, file_name[:-4])),
+                     left_ear, right_ear, left_eye, right_eye, nose, head),
                 ))
 
     with open("data_train.txt", 'w') as output_file:
